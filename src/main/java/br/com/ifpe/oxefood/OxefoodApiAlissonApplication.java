d9c36f9944bd59938428cf9c749ea43607bba59b
Alisson Gabriel Pereira de Lima <147565148+AlissonLimaG@users.noreply.github.com>
2025-04-03 20:50:30 -0300

    Enviando arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiAlissonApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiAlissonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiAlissonApplication.class, args);
+	}
+
+}
