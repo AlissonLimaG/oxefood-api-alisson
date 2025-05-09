abc08b63bf00d8c560dce8e48d412f199d58b65c
AlissonLimaG <alissongabriel010907@gmail.com>
2025-05-08 21:22:39 -0300

    rotas para listar, buscar e atualizar das entidades: cliente, entregador e produto

--- src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
@@ -1,14 +1,19 @@
 package br.com.ifpe.oxefood.api.entregador;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import br.com.ifpe.oxefood.modelo.entregador.Entregador;
 import br.com.ifpe.oxefood.modelo.entregador.EntregadorService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -21,10 +26,27 @@ public class EntregadorController {
 
     @PostMapping
     public ResponseEntity<Entregador> save(@RequestBody EntregadorRequest request) {
-        
+
         System.out.println(request);
         Entregador entregador = entregadorService.save(request.build());
         return new ResponseEntity<Entregador>(entregador, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Entregador> listarTodos() {
+        return entregadorService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Entregador obterPorID(@PathVariable Long id) {
+        return entregadorService.obterPorID(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> update(@PathVariable("id") Long id, @RequestBody EntregadorRequest request) {
+
+        entregadorService.update(id, request.build());
+        return ResponseEntity.ok().build();
     }
 
 }
--- src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -1,18 +1,44 @@
 package br.com.ifpe.oxefood.modelo.produto;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import jakarta.transaction.Transactional;
 
 @Service
 public class ProdutoService {
-    
+
     @Autowired
     private ProdutoRepository repository;
 
-    public Produto save(Produto produto){
+    public Produto save(Produto produto) {
 
         produto.setHabilitado(true);
-        
+
         return repository.save(produto);
+    }
+
+    public List<Produto> listarTodos() {
+
+        return repository.findAll();
+    }
+
+    public Produto obterPorID(Long id) {
+
+        return repository.findById(id).get();
+    }
+
+    @Transactional
+    public void update(Long id, Produto produtoAlterado) {
+
+        Produto produto = repository.findById(id).get();
+        produto.setCodigo(produtoAlterado.getCodigo());
+        produto.setTitulo(produtoAlterado.getTitulo());
+        produto.setDescricao(produtoAlterado.getDescricao());
+        produto.setValorUnitario(produtoAlterado.getValorUnitario());
+        produto.setTempoEntregaMaximo(produtoAlterado.getTempoEntregaMaximo());
+        produto.setTempoEntregaMinimo(produtoAlterado.getTempoEntregaMinimo());
+
+        repository.save(produto);
     }
 }
